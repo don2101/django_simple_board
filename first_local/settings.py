2c776a1a2ea68b192b21edec2ed4b495c3a84e5b
Hee-Don <don210198@gmail.com>
2019-03-14 10:23:18 +0900

    sns setting

--- first_local/settings.py
+++ first_local/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'board',
+    'sns',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
